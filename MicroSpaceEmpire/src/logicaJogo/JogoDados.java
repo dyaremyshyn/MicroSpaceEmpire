d9fff47de02b3c38e47602db5839bb435ff0fe50
dyaremyshyn <dmytroyaremyshyn@gmail.com>
2016-04-26 18:13:53 +0100

    jogoDados

--- MicroSpaceEmpire/src/logicaJogo/JogoDados.java
+++ MicroSpaceEmpire/src/logicaJogo/JogoDados.java
@@ -7,39 +7,103 @@ package logicaJogo;
 
 import logicaJogo.Cartas.Carta;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import logicaJogo.Cartas.*;
 
 /**
  *
  * @author Sergio
  */
 public class JogoDados implements Serializable {
-    List<Carta> nearSystem;
-    List<Carta> unalignedSystem;
-    List<Carta> imperio;
-    List<Carta> porConquistar;
+    private List<Carta> nearSystem;
+    private List<Carta> distantSystem;
+    private List<Carta> imperio;
+    private List<Carta> porConquistar;
+    private int pontuacao;
+    private int metal;
+    private int riqueza;
+    private int forcaMilitar;
     
     JogoDados(){
         
+        iniciar();
     }
-
+    
+    public void iniciar(){
+       imperio = new ArrayList<>();
+       nearSystem = new ArrayList<>();
+       distantSystem = new ArrayList<>();
+       
+       imperio.add(new HomeWorld());
+       
+       iniciaNearSystem();
+       iniciaDistantSystem();
+       
+       pontuacao=0;
+       metal=0;
+       riqueza=0;
+       forcaMilitar=0;
+    }
     
 
-    public void setNearSystem(List<Carta> nearSystem) {
-        this.nearSystem = nearSystem;
+    public void iniciaNearSystem() {
+        nearSystem.add(new Wolf359());
+        nearSystem.add(new Proxima());
+        nearSystem.add(new EpsilonEridani());
+        nearSystem.add(new Cygnus());
+        nearSystem.add(new TauCeti());
+        nearSystem.add(new Procyon());
+        nearSystem.add(new Sirius());
     }
 
-    public void setUnalignedSystem(List<Carta> unalignedSystem) {
-        this.unalignedSystem = unalignedSystem;
+    public void iniciaDistantSystem() {
+        distantSystem.add(new Canopus());
+        distantSystem.add(new Polaris());
+        distantSystem.add(new GalaxysEdge());
+    }
+    
+
+    public void setImperio(Carta e) {
+        imperio.add(e);
     }
 
-    public void setImperio(List<Carta> imperio) {
-        this.imperio = imperio;
+    public void setPorConquistar(Carta e) {
+        porConquistar.add(e);
     }
 
-    public void setPorConquistar(List<Carta> porConquistar) {
-        this.porConquistar = porConquistar;
+    public int getPontuacao() {
+        return pontuacao;
     }
+
+    public int getMetal() {
+        return metal;
+    }
+
+    public int getRiqueza() {
+        return riqueza;
+    }
+
+    public int getForcaMilitar() {
+        return forcaMilitar;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public void setMetal(int metal) {
+        this.metal = metal;
+    }
+
+    public void setRiqueza(int riqueza) {
+        this.riqueza = riqueza;
+    }
+
+    public void setForcaMilitar(int forcaMilitar) {
+        this.forcaMilitar = forcaMilitar;
+    }
+    
     
     
 }
