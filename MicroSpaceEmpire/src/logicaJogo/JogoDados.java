012234ea29280aa00d5ebe937169abd936f8bd4c
dyaremyshyn <dmytroyaremyshyn@gmail.com>
2016-05-02 19:58:18 +0100

    inicia atualizado

--- MicroSpaceEmpire/src/logicaJogo/JogoDados.java
+++ MicroSpaceEmpire/src/logicaJogo/JogoDados.java
@@ -64,6 +64,7 @@ public class JogoDados implements Serializable {
        
        iniciaNearSystem();
        iniciaDistantSystem();
+       iniciaTecnologia();
        
        atualizaRecursos();
        
