9294ad9ac15a48d7ddc5ad4131736180dd8a6a70
dyaremyshyn <dmytroyaremyshyn@gmail.com>
2016-06-09 20:08:05 +0100

    CONSTANTES

--- MicroSpaceEmpire/src/microspaceempire/gui/Constantes.java
+++ MicroSpaceEmpire/src/microspaceempire/gui/Constantes.java
@@ -24,7 +24,86 @@ public interface Constantes {
     static final int DIM_Y_SOUTH_PANEL = 260;
     
     
+    String CONOPUS = "Distant_Conopus";
+    String PATH_IMG_CONOPUS = "images/Distant_Conopus.png";
+    
+    String GALAYEDGE = "Distant_GalaxyEdge";
+    String PATH_IMG_GALAYEDGE = "images/Distant_GalaxyEdge.png";
+    
+    String POLARIS = "Distant_Polaris";
+    String PATH_IMG_POLARIS = "images/Distant_Polaris.png";
+       
+    String EVENT = "Event";
+    String PATH_IMG_EVENT = "images/Event.png";
+        
+    String ASTEROID = "Event_Asteroid";
+    String PATH_IMG_MAGGIE = "images/Event_Asteroid.png";
+    
+    String DERELICTSHIP = "Event_DerelictShip";
+    String PATH_IMG_DERELICTSHIP = "images/Event_DerelictShip.png";
+    
+    String LARGEINVASIONFORCE = "Event_LargeInvasionForce";
+    String PATH_IMG_LARGEINVASIONFORCE = "images/Event_LargeInvasionForce.png";
+    
+    String PEACEQUIET = "Event_PeaceQuiet";
+    String PATH_IMG_PEACEQUIET = "images/Distant_Polaris.png";
+       
+    String REVOLT = "Event_Revolt";
+    String PATH_IMG_REVOLT = "images/Event_Revolt.png";
+    
+    String REVOLT2 = "Event_Revolt1";
+    String PATH_IMG_REVOLT2 = "images/Event_Revolt1.png";
+    
+    String SMALLINVASIONFORCE = "Event_SmallInvasionForce";
+    String PATH_IMG_SMALLINVASIONFORCE = "images/Event_SmallInvasionForce.png";
+    
+    String STRIKE = "Event_Strike";
+    String PATH_IMG_STRIKE = "images/Event_Strike.png";
+    
+    String METAL = "Metal";
+    String PATH_IMG_METAL = "images/Metal.png";
+    
+    String MILITARY = "Military";
+    String PATH_IMG_MILITARY = "images/Military.png";
+    
+    String CYGNUS = "Near_Cygnus";
+    String PATH_IMG_CYGNUS = "images/Near_Cygnus.png";
+       
+    String EPSILONERIDANI = "Near_EpsilonEridani";
+    String PATH_IMG_EPSILONERIDANI = "images/Near_EpsilonEridani.png";
+        
+    String PROCYON = "Near_Procyon";
+    String PATH_IMG_ = "images/Near_.png";
     
     
+    String PROXIMA = "Near_Proxima";
+    String PATH_IMG_PROXIMA = "images/Near_Proxima.png";
+    
+    String SIRIUS = "Near_Sirius";
+    String PATH_IMG_SIRIUS = "images/Near_Sirius.png";
+    
+    String TAUCETI = "Near_TauCeti";
+    String PATH_IMG_TAUCETI = "images/Near_TauCeti.png";
+       
+    String WOLF359 = "Near_Wolf359";
+    String PATH_IMG_WOLF359 = "images/Near_Wolf359.png";
+    
+    String HOMEWORLD = "Starting_HomeWorld";
+    String PATH_IMG_HOMEWORLD = "images/Starting_HomeWorld.png";
+    
+    String SYSTEM = "System";
+    String PATH_IMG_SYSTEM = "images/System.png";
+    
+    String TECHNOLOGY = "Technology";
+    String PATH_IMG_TECHNOLOGY = "images/Technology.png";
+    
+    String WEALTH = "Wealth";
+    String PATH_IMG_WEALTH = "images/Wealth.png";
+    
+    String FUNDOINICIO = "fundoInicio";
+    String PATH_IMG_FUNDOINICIO = "images/fundoInicio.gif";
+    
+    String FUNDOPRINCIPAL = "fundoInicio";
+    String PATH_IMG_FUNDOPRINCIPAL = "images/fundoInicio.png";
     
 }
